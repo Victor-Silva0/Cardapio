c3ef8205fe3d99d2fd47d770acb69fb46c3a9138
Breno <71463305+brenopremoli@users.noreply.github.com>
2023-05-29 20:47:21 -0300

    Add files via upload

--- cardapio/index.php
+++ cardapio/index.php
@@ -1,5 +1,38 @@
-<?php require("header.php"); ?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="image/restaurante.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            document.querySelector('.toggle-menu').addEventListener('click', function () {
+                document.querySelector('.menu').classList.toggle('show');
+            });
+        });
+    </script>
+</head>
+
+
+<body>
+    <header>
+        <h1>Tech's Restaurante</h1>
+        <button class="toggle-menu">&#9776;</button>
+        <nav class="menu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="cardapio.php">Cardápio</a></li>
+                <li><a href="pedido.php">Pedido</a></li>
+                <li><a href="login.php">Administração</a></li>
+            </ul>
+        </nav>
+    </header>
     <article>
         <section class="s1">
             <h1>Tipos de Pratos</h1>
@@ -51,31 +84,44 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <a href="cardapio.html"><img src="image/1.jpg"
+                        <a href="cardapio.php"><img src="image/1.jpg"
                                 class="img-fluid smaller-image mx-auto d-block"></a>
 
-                        <a href="cardapio.html">Ver mais...</a>
+                        <a href="cardapio.php">Ver mais...</a>
                     </div>
                     <div class="col-md-6">
-                        <a href="cardapio.html"><img src="image/2.jpg"
+                        <a href="cardapio.php"><img src="image/2.jpg"
                                 class="img-fluid smaller-image mx-auto d-block"></a>
-                        <a href="cardapio.html">Ver mais...</a>
+                        <a href="cardapio.php">Ver mais...</a>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 text-center">
-                        <a href="cardapio.html"><img src="image/3.jpg"
+                        <a href="cardapio.php"><img src="image/3.jpg"
                                 class="img-fluid smaller-image mx-auto d-block"></a>
-                        <a href="cardapio.html">Ver mais...</a>
+                        <a href="cardapio.php">Ver mais...</a>
                     </div>
                     <div class="col-md-6">
-                        <a href="cardapio.html"><img src="image/4.jpg"
+                        <a href="cardapio.php"><img src="image/4.jpg"
                                 class="img-fluid smaller-image mx-auto d-block"></a>
-                        <a href="cardapio.html">Ver mais...</a>
+                        <a href="cardapio.php">Ver mais...</a>
                     </div>
                 </div>
             </div>
         </section>
     </article>
-   
-<?php require("footer.php"); ?>
+    <footer>
+        <div class="footer-content">
+          <div class="footer-item">
+            <h4>Endereço</h4>
+            <p>R. Teresina, 75 - Vila Paulo Roberto, Pres. Prudente - SP, 19046-230</p>
+          </div>
+          <div class="footer-item">
+            <h4>Contato</h4>
+            <p>Email: restaurante@gmail.com - Telefone: (18) 99999-9999</p>
+
+          </div>
+        </div>
+      </footer>
+</body>
+</html>
