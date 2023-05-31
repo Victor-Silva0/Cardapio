fc5f5d4c2c7759dd4cb5a193b058210d628dd276
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2023-05-31 12:24:17 -0300

    Add files via upload

---
+++ cardapio/update-comanda.php
@@ -0,0 +1,76 @@
+<?php
+require("header.php");
+
+require_once("connection.php");
+
+if (isset($_GET['idComanda']))
+{
+    $id_comanda = $_GET['idComanda'];
+    $mysql_query = "SELECT c.*, o.descricaoOrigem, s.descricao FROM comanda c, origem_comanda o, situacao_comanda s
+                        WHERE c.idComanda={$id_comanda} AND o.idOrigem=c.idOrigem AND s.idSituacao=c.idSituacao";
+    $result = $conn->query($mysql_query);
+    $row_placeholder = mysqli_fetch_array($result);
+}
+
+if (isset($_POST['atualizar']))
+{
+    $id_comanda = $_POST['id_comanda'];
+    $nome_cliente = $_POST['nome'];
+    $id_origem = $_POST['id_origem'];
+    $id_situacao = $_POST['id_situacao'];
+
+    $mysql_query = "UPDATE comanda c SET nomeClienteComanda='{$nome_cliente}', idOrigem='{$id_origem}', idSituacao='{$id_situacao}'
+                        WHERE c.idComanda={$id_comanda}";
+
+    $result = $conn->query($mysql_query);
+
+    mysqli_close($conn);
+
+    header("Location: comanda.php");
+}
+else
+{
+    $mysql_query = "SELECT * FROM origem_comanda ORDER BY idOrigem";
+    $select_origem = $conn->query($mysql_query);
+    $mysql_query = "SELECT * FROM situacao_comanda ORDER BY idSituacao";
+    $select_situacao = $conn->query($mysql_query);
+}
+
+?>
+
+<div class="container">
+    <br>
+    <h2>Comanda</h2>
+    <p>Atualização do cadastro da comanda.</p>
+    <hr>
+    <div class="wrapper">
+        <form method="post">
+            <input type="hidden" name="id_comanda" value="<?= $row_placeholder['idComanda']?>">
+            <label for="nome">Nome do cliente</label>
+            <input type="text" name="nome" class="form-control" style="width: 250px;" required value="<?= $row_placeholder['nomeClienteComanda']?>">
+            <br>
+            <label for="id_origem">Origem</label>
+            <select name="id_origem" class="form-control" style="width: 200px;" required>
+                <option value="<?= $row_placeholder['idOrigem'] ?>" selected><?= $row_placeholder['descricaoOrigem']; ?></option>
+                <?php while ($row_origem = mysqli_fetch_array($select_origem, MYSQLI_ASSOC)) {
+                    if ($row_origem['idOrigem'] != $row_placeholder['idOrigem']) {?>
+                        <option value="<?= $row_origem['idOrigem'];?>"><?= $row_origem['descricaoOrigem'];?></option>
+                <?php  } } ?>
+            </select>
+            <br>
+            <label for="id_situacao">Situação</label>
+            <select name="id_situacao" class="form-control" style="width: 200px;" required>
+                <option value="<?= $row_placeholder['idSituacao'] ?>" selected><?= $row_placeholder['descricao']; ?></option>
+                <?php while ($row_situacao = mysqli_fetch_array($select_situacao, MYSQLI_ASSOC)) {
+                    if ($row_situacao['idSituacao'] != $row_placeholder['idSituacao']) {?>
+                        <option value="<?= $row_situacao['idSituacao'];?>"><?= $row_situacao['descricao'];?></option>
+                <?php  } } ?>
+            </select>
+            <br>
+            <input type="submit" name="atualizar" value="Atualizar" class="btn btn-primary w100">
+        </form>
+    </div>
+</div>
+
+<br>
+<?php require("footer.php"); ?>
