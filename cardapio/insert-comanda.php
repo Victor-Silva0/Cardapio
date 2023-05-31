b0941aada0aebaae1a913507156cbda5e5a7b119
RafaelFernandes1992 <91971591+RafaelFernandes1992@users.noreply.github.com>
2023-05-31 14:33:21 -0300

    Add files via upload

---
+++ cardapio/insert-comanda.php
@@ -0,0 +1,99 @@
+<?php
+require("header.php");
+
+require_once("connection.php");
+
+if (isset($_GET['idComanda']))
+{
+    $id_comanda = $_GET['idComanda'];
+    $mysql_query = "SELECT ic.*, oc.*, c.nomeClienteComanda
+    FROM itens_comanda ic, opcoes_cardapio oc, comanda c
+    WHERE ic.idOpcaoCardapio=oc.idOpcaoCardapio
+    AND c.idComanda=ic.idComanda 
+    AND ic.idComanda={$id_comanda}
+    order by idItemComanda";
+    $selectComanda = $conn->query($mysql_query);
+    $row_comanda = mysqli_fetch_array($selectComanda);
+}
+
+if (isset($_POST['cadastrar']))
+{
+    $id_comanda = $_POST['id_comanda'];
+    $nome = $_POST['nome'];
+    $id_situacao = $_POST['id_situacao'];
+    $id_origem = $_POST['id_origem'];
+
+    $mysql_query = "INSERT INTO `comanda`(`idComanda`, `nomeClienteComanda`, `idOrigem`, `idSituacao`) 
+    VALUES (
+    '{$id_comanda}', 
+    '{$nome}', 
+    '{$id_situacao}', 
+    '{$id_origem}'
+    )";
+
+    $insertItensComanda = $conn->query($mysql_query);
+
+    header("Location: comanda.php");
+    mysqli_close($conn);    	
+}
+else
+{
+    $mysql_query = "SELECT * FROM opcoes_cardapio ORDER BY idOpcaoCardapio";
+    $selectOpCardapio = $conn->query($mysql_query);
+    $mysql_query1 = "SELECT * FROM origem_comanda";
+    $selectOrigem = $conn->query($mysql_query1);
+    $mysql_query2 = "SELECT * FROM situacao_comanda";
+    $selectSituacao = $conn->query($mysql_query2);
+
+}
+
+?>
+
+<div class="container">
+    <br>
+    <h2>Nova Comanda</h2>
+    <p>Cadastro de comanda</p>
+    <hr>
+    <br>
+    <div class="wrapper">
+        <form method="post">
+            <input type="hidden" name="id_comanda" value="<?= $row_comanda['idComanda']?>">
+            
+            <div class="row">
+                <div class="col-4">
+                    <label for="nome">Nome do cliente</label>
+                    <input type="text" name="nome" class="form-control" required>
+                </div>
+                <div class="col-4">  
+                    <label for="id_origem">Origem</label>
+                    <select class="form-select" name="id_origem" required>
+                        <option selected>Selecione...</option>
+                        <?php while ($row_origem = mysqli_fetch_array($selectOrigem, MYSQLI_ASSOC)) { ?>
+                        <option value="<?= $row_origem['idOrigem'];?>"><?= $row_origem['descricaoOrigem'];?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="col-4">  
+                    <label for="id_situacao">Situação</label>
+                    <select class="form-select" name="id_situacao" required>
+                        <option selected>Selecione...</option>
+                        <?php while ($row_situacao = mysqli_fetch_array($selectSituacao, MYSQLI_ASSOC)) { ?>
+                        <option value="<?= $row_situacao['idSituacao'];?>"><?= $row_situacao['descricao'];?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+
+            </div>
+            <br>
+            <input type="submit" name="cadastrar" value="Cadastrar" class="btn btn-primary">
+        </form>
+        <br>
+        <br>
+        
+
+
+    </div>
+</div>
+
+<br>
+<?php require("footer.php"); ?>
