179f66783cb044196d284d94ff1fe76376672eb3
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2023-06-10 00:27:56 -0300

    dashboard com mensagem sobre a utilidade da conta

--- cardapio/dashboard.php
+++ cardapio/dashboard.php
@@ -13,7 +13,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <p>Olá, <b><?php echo htmlspecialchars($_SESSION["nomeUsuario"]); ?></b>. Seja bem-vindo!</h1>
   <hr>  
   <p>&nbsp;</p>
-  <p>As alterações são realizadas aqui!!!</p>
+  <strong>Conta de gerenciamento</strong><br><br>
+  <p class="alert alert-info">
+    Enquanto logado na conta, o administrador pode realizar o gerenciamento das comandas,<br>
+    dos itens das comandas, gerenciar opções do cardápio, tipos de opções<br>
+    e também tem controle sobre os comentários dos clientes!
+  </p>
   <p>
     <a href="reset-senha.php" class="btn btn-warning">Resete a sua senha</a>
     <a href="logout.php" class="btn btn-danger ml-3">Faça logout em sua conta</a>
